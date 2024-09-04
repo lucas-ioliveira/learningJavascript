a559d0709fcb2cbd9e7065513bbcbbaf2f644209
Lucas Oliveira <lucasio2008@gmail.com>
2024-09-04 18:28:38 -0300

    first commit

---
+++ tipos_dados/comparacao_boolean/script.js
@@ -0,0 +1,22 @@
+/*
+Maior que: >
+Menor que: <
+Maior ou igual a: >=
+Menor ou igual a: <=
+Igual a: ==
+Diferente de: !=
+Identico a: ===
+
+*/
+
+// ex:
+
+console.log( typeof (10 > 5))
+console.log(10 < 5)
+console.log(10 >= 5)
+console.log(10 <= 5)
+console.log(10 == 5)
+console.log(10 != 5)
+console.log(10 === 5)
+
+// Nesse caso o retorno é true ou false, tipo boolean
