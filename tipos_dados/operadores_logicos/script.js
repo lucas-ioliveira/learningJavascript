47ee84f3bcbb0956cedebc33e76055a6ffadc66b
Lucas Oliveira <lucasio2008@gmail.com>
2024-09-04 18:56:53 -0300

    feat: Aula sobre operadores lógicos

---
+++ tipos_dados/operadores_logicos/script.js
@@ -0,0 +1,16 @@
+/*
+&& ou and : E -> Para ser true os dois operandos precisam ser true
+|| ou or : OU -> Para ser true pelo menos um dos operandos precisa ser true
+! ou not : NÃO -> Para ser false o operando precisa ser true
+*/
+
+// ex:
+
+console.log(true && true);
+console.log(true && false);
+console.log(false || false);
+console.log(!false);
+
+
+
+// Nesse caso o retorno é true ou false, tipo boolean
