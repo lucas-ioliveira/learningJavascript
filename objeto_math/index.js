86157e8fe0946424161aca035de926173d65b945
Lucas Oliveira <lucasio2008@gmail.com>
2024-09-05 10:27:42 -0300

    feat:Aula sobre math

---
+++ objeto_math/index.js
@@ -0,0 +1,26 @@
+/**
+ * Objeto dispolibilizado pela liguagem para operações matematicas
+ */
+
+// ex:
+
+let num1 = 9.54578;
+
+let num2 = Math.floor(num1); // arredonda para baixo
+console.log(num2);
+
+let num3 = Math.ceil(num1); // arredonda para cima
+console.log(num3);
+
+let num4 = Math.round(num1); // arredonda para o mais proximo
+console.log(num4);
+
+console.log(Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)); // retorna o maior
+console.log(Math.min(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)); // retorna o menor
+
+console.log(Math.random()); // gera um numero aleatorio
+
+console.log(Math.PI); // retorna o valor de PI
+
+console.log(Math.pow(2, 3)); // retorna o valor de 2 elevado a 3
+
